c4637a65f0af38aae99cc6e1555ad72c686beee1
João Augusto Rezende <joaoaugustofrezende@outlook.com>
2025-11-23 21:32:14 -0300

    feat: Rotas de monitoramento prontas

--- routes/monitoring/monitoring.routes.js
+++ routes/monitoring/monitoring.routes.js
@@ -2,118 +2,43 @@ const express = require("express");
 const router = express.Router();
 
 const Monitoring = require("../../models/monitoring/monitoring.model");
-
-async function saveMonitoring(monitoringID, data) {
-    const monitoring = await Monitoring.findByIdAndUpdate(
-        monitoringID,
-        data
-    );
-    return monitoring;
-}
-
-async function getPersonByID(monitoringID) {
-    const monitoring = await Monitoring.findById(monitoringID).populate("monitored").exec();
-    return monitoring;
-}
+const Person = require("../../models/person/person.model");
 
 router.post("/", async (req, res) => {
     try {
         const monitoring = req.body;
-        const responsibleID = monitoring?.responsavelId;
-        delete monitoring.responsavelId
 
-        const newPerson = await Monitoring.create(monitoring);
+        const newMonitoring = await Monitoring.create(monitoring);
 
-        if (!newPerson) {
-            res.status(500).json({ error: "Erro ao criar nova pessoa" });
+        if (!newMonitoring) {
+            res.status(500).json({ error: "Erro ao criar novo registro de incidente" });
         }
 
-        if (responsibleID) {
-            const responsible = await getPersonByID(responsibleID);
-            responsible.monitored.push(newPerson);
-            await savePerson(responsibleID, responsible);
-        }
-
-        res.status(200).json({ message: "Pessoa criada com sucesso", data: newPerson });
+        res.status(200).json({ message: "Registro de incidente criado com sucesso", data: newMonitoring });
     } catch (err) {
-        res.status(500).json({ error: "Erro ao criar pessoa", message: err });
+        res.status(500).json({ error: "Erro ao criar incidente", message: err });
     }
 });
 
-router.put("/:personid", async (req, res) => {
+router.get("/getMonitoringByResponsibleID/:responsibleid", async (req, res) => {
     try {
-        const monitoring = { ...req.body };
-        const monitoringID = req.params.personid;
-        const updatedPerson = await savePerson(
-            monitoringID,
-            monitoring
-        );
-        if (!updatedPerson) {
-            res.status(400).json({ error: "Pessoa não existe" });
-            return;
-        }
-        res.status(200).json({ message: "Editado com sucesso!", data: updatedPerson });
+        const personID = req.params.responsibleid;
+        const person = await Person.findById(personID);
+        const IDsOfThoseMonitored = person?.monitored;
+        const monitorings = await Monitoring.find({ monitored: { $in: IDsOfThoseMonitored } }).populate("monitored").exec();
+        res.status(200).json(monitorings);
     } catch (err) {
-        res.status(500).json({ error: "Erro ao editar pessoa", message: err });
+        res.status(500).json({ error: "Erro ao buscar incidentes", message: err });
     }
 });
 
-router.get("/", async (req, res) => {
+router.get("/getMonitoringByPersonID/:personid", async (req, res) => {
     try {
-        const people = await Monitoring.find({});
-        res.status(200).json(people);
-    } catch (err) {
-        res.status(500).json({ error: "Erro ao buscar pessoas", message: err });
-    }
-});
-
-router.get("/getPersonByID/:personid", async (req, res) => {
-    try {
-        const monitoringID = req.params.personid;
-        const monitoring = await getPersonByID(monitoringID);
+        const personID = req.params.personid;
+        const monitoring = await Monitoring.find({ monitored: personID }).populate("monitored").exec();
         res.status(200).json(monitoring);
     } catch (err) {
-        res.status(500).json({ error: "Erro ao buscar pessoa", message: err });
-    }
-});
-
-router.get("/getMonitored/:personid", async (req, res) => {
-    try {
-        const monitoringID = req.params.personid;
-        const monitoring = await getPersonByID(monitoringID);
-        const monitored = monitoring.monitored;
-        res.status(200).json(monitored);
-    } catch (err) {
-        res.status(500).json({ error: "Erro ao buscar pessoa", message: err });
-    }
-});
-
-router.delete("/:personid", async (req, res, next) => {
-    try {
-        const monitoringID = req.params?.personid;
-        const deletedPerson = await Monitoring.findByIdAndDelete(monitoringID);
-        if (!deletedPerson) {
-            res.status(400).send({
-                error: "Erro ao deletar, pessoa não encontrada",
-            });
-            return;
-        }
-
-        let responsible = await Monitoring.findOne({ monitored: monitoringID });
-        if (responsible) {
-            responsible.monitored = responsible.monitored.filter(prs => prs != monitoringID);
-            await savePerson(
-                responsible._id,
-                responsible
-            );
-        }
-
-        res.status(200).json({
-            message: "Pessoa deletada com sucesso",
-            data: deletedPerson,
-        });
-    } catch (err) {
-        res.status(500).send({ error: "Erro ao deletar pessoa", message: err });
+        res.status(500).json({ error: "Erro ao buscar incidente da pessoa", message: err });
     }
 });
 
